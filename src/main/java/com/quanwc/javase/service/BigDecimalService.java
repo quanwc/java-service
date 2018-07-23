64f09227f54d75344958fca7de2f8f03bbd47fb4
quanwenchao <quan_wc@163.com>
2018-07-23 19:11:05 +0800

    start BigDecimal类

---
+++ src/main/java/com/quanwc/javase/service/BigDecimalService.java
@@ -0,0 +1,12 @@
+package com.quanwc.javase.service;
+
+/**
+ * BigDecimal类的使用
+ * Created by quanwenchao
+ * 2018/7/23 18:58:59
+ */
+public class BigDecimalService {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/quanwc/web/GJavaApplication.java
@@ -0,0 +1,12 @@
+package com.quanwc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GJavaApplication.class, args);
+	}
+}
